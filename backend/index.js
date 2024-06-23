902a887a81bc5ffa84885a415d4032c26297439a
avinasdube <avidubey712@gamil.com>
2024-06-23 15:58:32 +0530

    [refactor/reconfigure] : reconfigured server as per client needs

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from 'cors';
+import { join } from "path";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -15,6 +16,7 @@ const io = new Server(httpServer, {
 })
 
 let connections = [];
+let activeRooms = [];
 
 io.on('connect', (socket) => {
 
@@ -23,31 +25,42 @@ io.on('connect', (socket) => {
     io.emit('updateUserList', connections);
 
     // an event to create and join a room at first place
-    socket.on('create', (room) => {
-        console.log(`${socket.id} created : ${room}`);
-        socket.join(room);
+    socket.on('createRoom', ({ currentUser, room }) => {
+        console.log(`${currentUser} created : ${room.roomName}`);
+        socket.join(room.roomName);
+        activeRooms.push(room);
         socket.emit('roomCreated', room); // informing creator that room is created.
     })
 
+    // send the list of active rooms to client
+    socket.emit('activeRooms', activeRooms);
+
     // an event to let others join a room
-    socket.on('join', (room) => {
-        console.log(`${socket.id} created : ${room}`);
-        socket.join(room);
+    socket.on('joinRoom', (roomName) => {
+        console.log(`${socket.id} joined : ${roomName}`);
+        socket.join(roomName);
     })
 
+    // an event to fetch historical messages for a room
+    socket.on('getMessages', (roomName) => {
+        // Assuming messages are stored in an array per room on the server
+        const historicalMessages = activeRooms.find(room => room.roomName === roomName)?.messages || [];
+        socket.emit('messages', historicalMessages);
+    });
+
     // an event to broadcast the message to all members of same room
-    socket.on('message', ({ room, message }) => {
-        console.log(`Message received in room ${room}: `, message);
-        io.to(room).emit('message', message); // Broadcast the message to all members in the room
+    socket.on('message', ({ currentRoom, message }) => {
+        console.log(`Message received in room ${currentRoom}: `, message);
+        io.to(currentRoom).emit('message', message); // Broadcast the message to all members in the room
     });
 
     // an event to show disconnected users
     socket.on('disconnect', () => {
-        connections = connections.filter(conn => conn !== socket.id);
-        console.log("Just Disconnected : ", socket.id)
-        console.log("Currently remaining connections : ", connections);
-        io.emit('updateduserList', connections);
-    })
+        connections = connections.filter(id => id !== socket.id);
+        console.log("Just Disconnected:", socket.id);
+        console.log("Currently remaining connections:", connections);
+        io.emit('updateUserList', connections);
+    });
 })
 
 httpServer.listen(PORT, () => {
